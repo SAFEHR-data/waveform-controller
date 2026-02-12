b1baf118097bc1a6959458e7697d22daa3a80a3c
Jeremy Stein <j.stein@ucl.ac.uk>
2026-02-12 19:45:00 +0000

    Parameterize test and check the right files are being returned

--- tests/test_snakemake.py
+++ tests/test_snakemake.py
@@ -1,6 +1,8 @@
 from datetime import timedelta, datetime, timezone
 from pathlib import Path
 
+import pytest
+
 import locations
 
 import src.pipeline.utils as utils
@@ -86,34 +88,39 @@ def _make_test_input_csv(csv_dir: Path):
     return [f.get_orig_csv() for f in files]
 
 
-def test_determine_eventual_outputs(tmp_path: Path, monkeypatch):
+@pytest.mark.parametrize(
+    "process_only_yesterday, process_datestring, expected_file_indexes",
+    [
+        # with process only yesterday true we should return only the single file from yesterday
+        (True, "", [1]),
+        # with process only yesterday false and an empty process_datestring we should return all 4 files.
+        (False, "", [0, 1, 2, 3]),
+        # with process only yesterday false and process_datestring set to two days ago we should return the two files from two days ago
+        (
+            False,
+            (datetime.now(tz=timezone.utc).date() - timedelta(days=2)).isoformat(),
+            [2, 3],
+        ),
+    ],
+)
+def test_determine_eventual_outputs(
+    tmp_path: Path,
+    monkeypatch,
+    process_only_yesterday,
+    process_datestring,
+    expected_file_indexes,
+):
     original_csv_dir = tmp_path / locations.WAVEFORM_ORIGINAL_CSV.relative_to("/")
     expected_paths = _make_test_input_csv(original_csv_dir)
     monkeypatch.setattr(
-        utils,
-        "CSV_PATTERN",
-        original_csv_dir / (locations.FILE_STEM_PATTERN + ".csv")
+        utils, "CSV_PATTERN", original_csv_dir / (locations.FILE_STEM_PATTERN + ".csv")
     )
     monkeypatch.setattr("src.pipeline.utils.hash_csn", mock_do_hash)
     csv_wait_time = timedelta(0)
-    # with process only yesterday true we should return only the single file from yesterday
-    process_only_yesterday = True
-    process_datestring = ""
+
     files, hash_to_csn = utils.determine_eventual_outputs(
         csv_wait_time, process_only_yesterday, process_datestring
     )
-    assert len(files) == 1
-    # with process only yesterday false and an empty process_datestring we should return all 4 files.
-    process_only_yesterday = False
-    process_datestring = ""
-    files, hash_to_csn = utils.determine_eventual_outputs(
-        csv_wait_time, process_only_yesterday, process_datestring
-    )
-    assert len(files) == 4
-    # with process only yesterday false and process_datestring set to two days ago we should return the two files from two days ago
-    process_only_yesterday = False
-    process_datestring = datetime.strftime(datetime.now() - timedelta(2), "%Y-%m-%d")
-    files, hash_to_csn = utils.determine_eventual_outputs(
-        csv_wait_time, process_only_yesterday, process_datestring
-    )
-    assert len(files) == 2
+    assert {
+        str(f.get_original_csv_path().relative_to(original_csv_dir)) for f in files
+    } == {expected_paths[ex_i] for ex_i in expected_file_indexes}
