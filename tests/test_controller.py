d33fc7a106f3b18f3795aa816e4261f2d93afaa8
Jeremy Stein <j.stein@ucl.ac.uk>
2026-02-13 17:38:05 +0000

    Add test cases for DB connection failure and bad messages

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -8,15 +8,23 @@ from controller import WaveformController
 
 
 @pytest.mark.parametrize(
-    "opt_out, expected_accept",
-    [
-        [True, False],
-        [False, True],
-    ],
+    "opt_out",
+    [True, False],
 )
-def test_controller_callback(monkeypatch, opt_out, expected_accept):
+@pytest.mark.parametrize(
+    "db_connect_failure",
+    [True, False],
+)
+@pytest.mark.parametrize(
+    "bad_data",
+    [True, False],
+)
+def test_controller_callback(monkeypatch, opt_out, db_connect_failure, bad_data):
     emap_db_mock = Mock()
-    emap_db_mock.get_row.return_value = ("mrn", "nhsno", "csn", opt_out)
+    if db_connect_failure:
+        emap_db_mock.get_row.side_effect = ConnectionError("mock database error")
+    else:
+        emap_db_mock.get_row.return_value = ("mrn", "nhsno", "csn", opt_out)
     monkeypatch.setattr("controller.db.starDB", Mock(return_value=emap_db_mock))
 
     write_frame_mock = Mock(return_value=True)
@@ -32,6 +40,9 @@ def test_controller_callback(monkeypatch, opt_out, expected_accept):
         "unit": "uV",
         "numericValues": "[1,2,3]",
     }
+    if bad_data:
+        # simulate a missing key
+        del fake_data["sourceChannelId"]
     fake_data_str = json.dumps(fake_data)
     controller = WaveformController()
 
@@ -43,10 +54,28 @@ def test_controller_callback(monkeypatch, opt_out, expected_accept):
 
     controller.waveform_callback(channel_mock, method_frame_mock, None, fake_data_str)
 
-    emap_db_mock.get_row.assert_called_once()
-    if expected_accept:
-        write_frame_mock.assert_called()
-        channel_mock.basic_ack.assert_called_once_with(delivery_tag)
-    else:
+    if not bad_data:
+        # we at least tried to query the DB
+        emap_db_mock.get_row.assert_called_once()
+
+    if bad_data:
+        write_frame_mock.assert_not_called()
+        # db should not even have been queried if data was bad
+        emap_db_mock.get_row.assert_not_called()
+        channel_mock.basic_reject.assert_called_once_with(delivery_tag, False)
+        channel_mock.basic_ack.assert_not_called()
+    elif db_connect_failure:
+        # if the DB lookup failed, we should not write anything and requeue the message
+        write_frame_mock.assert_not_called()
+        channel_mock.basic_reject.assert_called_once_with(delivery_tag, True)
+        channel_mock.basic_ack.assert_not_called()
+    elif opt_out:
+        # patient has opted out, dump the message
         write_frame_mock.assert_not_called()
         channel_mock.basic_reject.assert_called_once_with(delivery_tag, False)
+        channel_mock.basic_ack.assert_not_called()
+    else:
+        # happy path
+        write_frame_mock.assert_called_once()
+        channel_mock.basic_reject.assert_not_called()
+        channel_mock.basic_ack.assert_called_once_with(delivery_tag)
