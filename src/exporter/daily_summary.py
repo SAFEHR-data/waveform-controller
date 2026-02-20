bf628e43370a92c1ee023442c3614a52af3418b2
Jeremy Stein <j.stein@ucl.ac.uk>
2026-02-20 11:15:59 +0000

    Linting fixes

--- src/exporter/daily_summary.py
+++ src/exporter/daily_summary.py
@@ -5,19 +5,24 @@ from exporter.parquet import parquet_min_max_value
 
 logger = logging.getLogger(__name__)
 
+
 def make_daily_hash_summary(daily_files, out_json_file):
-    min_timestamp_key = 'min_timestamp'
-    max_timestamp_key = 'max_timestamp'
+    min_timestamp_key = "min_timestamp"
+    max_timestamp_key = "max_timestamp"
     hash_summary_by_csn = {}
     for daily_file in daily_files:
         entry = {}
         original_parquet = daily_file.get_original_parquet_path()
         entry["csn"] = daily_file.csn
         entry["hashed_csn"] = daily_file.hashed_csn
-        min_timestamp, max_timestamp = parquet_min_max_value(original_parquet, "timestamp")
+        min_timestamp, max_timestamp = parquet_min_max_value(
+            original_parquet, "timestamp"
+        )
         if min_timestamp is None or max_timestamp is None:
             # do not contribute to stats
-            logger.warning(f"Parquet does not have a min/max value, assumed to be empty: {original_parquet}")
+            logger.warning(
+                f"Parquet does not have a min/max value, assumed to be empty: {original_parquet}"
+            )
             break
         entry[min_timestamp_key] = min_timestamp
         entry[max_timestamp_key] = max_timestamp
@@ -27,8 +32,12 @@ def make_daily_hash_summary(daily_files, out_json_file):
         else:
             # update the limits (there can be multiple files for the same CSN because each variable/channel
             # is in its own file)
-            existing_entry[min_timestamp_key] = min(min_timestamp, existing_entry[min_timestamp_key])
-            existing_entry[max_timestamp_key] = max(max_timestamp, existing_entry[max_timestamp_key])
+            existing_entry[min_timestamp_key] = min(
+                min_timestamp, existing_entry[min_timestamp_key]
+            )
+            existing_entry[max_timestamp_key] = max(
+                max_timestamp, existing_entry[max_timestamp_key]
+            )
 
     hash_summary = list(hash_summary_by_csn.values())
 
