cba9b54c1ab8018b209827d0f0cc3e5bd64cead2
Stephen Thompson <s.thompson@ucl.ac.uk>
2026-08-27 14:47:37 +0100

    moved sql and implemented lab results

--- src/sql/lab_results.sql
+++ src/sql/lab_results.sql
@@ -8,11 +8,11 @@
 SELECT
     r.result_last_modified_datetime AS DateTimeRecorded,
 
-    MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '1001') AS "C-reactive protein",
+    MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '1001') AS "C-reactive protein 1",
     MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '390793054') AS "CSF WCC TUBE 1",
     MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '390793057') AS "CSF WCC TUBE 2",
     MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '390793060') AS "CSF WCC TUBE 3",
-    MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '722790196') AS "C-reactive protein"
+    MAX(r.value_as_real) FILTER (WHERE r.lab_test_definition_id = '722790196') AS "C-reactive protein 2"
 
     r.units AS Units,
     r.abnormal_flag AS Abnormal_result,
@@ -29,7 +29,7 @@ r.lab_test_definition_id IN ('1001',
                              '390793057', 
                              '390793060', 
                              '722790196')
-AND vo.valid_from BETWEEN %(yesterday)s AND %(today)s
+		AND vo.valid_from BETWEEN %(start_datetime)s AND %(end_datetime)s
 AND o.hospital_visit_id = %(hospital_visit_id)s
 
 GROUP BY DateTimeRecorded, Units, Abnormal_result, Comments
